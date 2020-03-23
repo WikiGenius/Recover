e39d0c8f27962f7b5b58dd3ad6acf22a364fe0d2
Muhammed El_Yamani <muhammedelyamani92@gmail.com>
2020-03-24 00:59:49 +0200

    save2

--- test.py
+++ test.py
@@ -1,36 +1,91 @@
+from goto import with_goto
+
+# variables
 path = "card.raw"
+count = 0
 byte_list = []
 
 
-def look_sig_jpg(card, byte_list):
-    n = len(byte_list)
-    for _ in range(4 - n):
+
+def fill_byte_list(card, byte_list):
+    for _ in range(4):
         byte_list.append(card.read(1))
-    return byte_list
 
 
-def sig_beg_jpg(byte_list):
-    if len(byte_list == 4):
-        pass
-    return
+def write_header_file(image, byte_list):
+    for i in range(4):
+        image.write(byte_list[i])
 
 
-def open_new_jpeg():
-    # write byte_list?
-    # write 512 bytes
-    # byte_list = look_sig_jpg()
-    return False
+def add_element_byte_list(card, byte_list):
+    byte_list.reverse()
+    byte_list.pop()
+    byte_list.reverse()
+    byte_list.append(card.read(1))
 
 
-with open(path, "rb") as card:
-    byte_list = look_sig_jpg(card, byte_list)
-    if sig_beg_jpg(byte_list):
-        # open new jpeg file
-        while True:
-            end_flag = open_new_jpeg()
-            if end_flag:
-                break
-    else:
-        # pop last byte in byte list
-        # and loop again
-        pass
+@with_goto
+def test():
+    # 1 open card file
+    with open(path, "rb") as card:
+        print("step1")
+        # 2 read 4 bytes into byte_list
+        print("step2")
+
+        fill_byte_list(card, byte_list)
+        # 3 compare with signature
+        print("step3")
+
+        label .trig
+        if int(byte_list[0].hex()) == 0xff and int(byte_list[1].hex()) == 0xd8 and int(byte_list[2].hex()) == 0xff and int(byte_list[3].hex()) & 0xf0 == 0xe0:
+            # 4if yes
+            print("step4")
+
+            label .new_image
+
+            # 5open new image jpg
+            with open("{:3d}.jpg".format(count), "wb") as image:
+                print("step5")
+
+                count += 1
+                write_header_file(image, byte_list)
+                # 6clear byte_list
+
+                byte_list.clear()
+                print("step6")
+
+                label .continue_write
+                # 7write 512 bytes into image
+                image.write(card.read(512))
+                print("step7")
+
+                # 8 read 4 bytes into byte_list
+
+                fill_byte_list(card, byte_list)
+                print("step8")
+                # 9check end card file
+
+                if byte_list[0] == b'':
+                    goto .end
+                print("step9")
+                
+                # 10 compare with signature
+                if int(byte_list[0].hex()) == 0xff and int(byte_list[1].hex()) == 0xd8 and int(byte_list[2].hex()) == 0xff and int(byte_list[3].hex()) & 0xf0 == 0xe0:
+                    # 11 if yes go to step 5
+                    goto .new_image
+                else:
+                    # 12 if no go to step 7
+                    goto .continue_write
+        else:
+            # 13 if no
+            # 14 check end card file
+            if byte_list[0] == b'':
+                goto .end
+            # 15 drop first byte and append new byte (overlap)
+            add_element_byte_list(card, byte_list)
+            # goto step 3
+            goto .trig
+    label .end
+
+
+test()
