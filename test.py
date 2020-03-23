cd6f7e65759bb80c09d15b0cf1aec9bc920b04f2
Muhammed El_Yamani <muhammedelyamani92@gmail.com>
2020-03-23 15:01:07 +0200

    save

--- test.py
+++ test.py
@@ -1,25 +1,36 @@
 path = "card.raw"
-byte1 = 'ff'
-byte2 = 'd8'
-byte3 = 'ff'
-byte4 = ['e0', 'e1', 'e2', 'e3', 'e4', 'e5', 'e6',
-         'e7', 'e8', 'e9', 'ea', 'eb', 'ec', 'ed',
-         'ee',  'ef']
 byte_list = []
-sig = byte1+byte2+byte3+'e'
-start = 0
-count = 0
-flags = []
-txt = ""
-with open(path, "rb") as card:
-    txt = str(card.read().hex())
-    # print(txt)
-    while True:
-        flag = txt[start:].find(sig)
-        if flag == -1:
-            break
-        start += flag + 1
-        flags.append(flag)
 
-with open("{:03d}.jpg".format(count),"wb") as image:
-    image.write(bytes( txt[flags[0]:flags[1]]))
+
+def look_sig_jpg(card, byte_list):
+    n = len(byte_list)
+    for _ in range(4 - n):
+        byte_list.append(card.read(1))
+    return byte_list
+
+
+def sig_beg_jpg(byte_list):
+    if len(byte_list == 4):
+        pass
+    return
+
+
+def open_new_jpeg():
+    # write byte_list?
+    # write 512 bytes
+    # byte_list = look_sig_jpg()
+    return False
+
+
+with open(path, "rb") as card:
+    byte_list = look_sig_jpg(card, byte_list)
+    if sig_beg_jpg(byte_list):
+        # open new jpeg file
+        while True:
+            end_flag = open_new_jpeg()
+            if end_flag:
+                break
+    else:
+        # pop last byte in byte list
+        # and loop again
+        pass
