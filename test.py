fa6d73a4fc45bd23e5c0e1e9d15d3b2562868684
Muhammed El_Yamani <muhammedelyamani92@gmail.com>
2020-03-24 16:52:02 +0200

    save5

--- test.py
+++ test.py
@@ -1,60 +1,29 @@
 from goto import with_goto
-
-# variables
 path = "card.raw"
 count = 0
 
-
-def read_block(card):
-    block = card.read(512)
-    return block
-
-
+# 1 open card file
 @with_goto
 def test(count):
-    # 1 open card file
     with open(path, "rb") as card:
-        # 2 read 512 bytes into block
-        block = read_block(card)
-        # 3 compare with signature
-        label .trig
-        (byte1, byte2, byte3, byte4) = (block[0], block[1], block[2], block[3])
-        if byte1 == 0xff and byte2 == 0xd8 and byte3 == 0xff and byte4 & 0xf0 == 0xe0:
-            # 4if yes
-            label .new_image
-
-            # 5open new image jpg
-            with open("images/{:03d}.jpg".format(count), "wb") as image:
+        while True:
+            # 2 read block 512 byte
+            block = card.read(512)
+            # 3 check end file card
+            if(len(block) == 0):
+                break
+            # 4 the header block
+            (b1, b2, b3, b4) = (block[0], block[1], block[2], block[3])
+            # 5 check the header with signature jpg
+            if b1 == 0xff and b2 == 0xd8 and b3 == 0xff and b4 & 0xf0 == 0xe0:
                 count += 1
-                label .continue_write
-                # 6write 512 bytes into image
-                image.write(block)
-                # 7 read 512 bytes into block
-                block = read_block(card)
-                # 9check end card file
-                if len(block) < 512:
-                    # goto .end
-                    return
-
-                # 10 compare with signature
-                (byte1, byte2, byte3, byte4) = (
-                    block[0], block[1], block[2], block[3])
-                if byte1 == 0xff and byte2 == 0xd8 and byte3 == 0xff and byte4 & 0xf0 == 0xe0:
-                    # 11 if yes go to step 5
-                    goto .new_image
-                else:
-                    # 12 if no go to step 7
-                    goto .continue_write
-        else:
-            block = read_block(card)
-            # 13 if no
-            # 14 check end card file
-            if block[0] == b'':
-                # goto .end
-                return
-            # go to step 3
-            goto .trig
-        # label .end
-
-
+                # 6 yes -> open new image jpg
+                label .loop
+                with open("{:03d}.jpg".format(count),"ab") as image:
+                    #7 write into image
+                    image.write(block)
+            else:
+                if count > 0:
+                    # go to step 6
+                    goto .loop
 test(count)
