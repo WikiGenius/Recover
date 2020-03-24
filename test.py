8f3e6a2d4ff0edc60c126eaa8722116ff791a3dd
Muhammed El_Yamani <muhammedelyamani92@gmail.com>
2020-03-24 14:53:31 +0200

    save3

--- test.py
+++ test.py
@@ -6,10 +6,11 @@ count = 0
 byte_list = []
 
 
-
 def fill_byte_list(card, byte_list):
+    byte_list.clear()
     for _ in range(4):
-        byte_list.append(card.read(1))
+        byte = card.read(1)
+        byte_list.append(byte)
 
 
 def write_header_file(image, byte_list):
@@ -21,56 +22,76 @@ def add_element_byte_list(card, byte_list):
     byte_list.reverse()
     byte_list.pop()
     byte_list.reverse()
-    byte_list.append(card.read(1))
+    byte = card.read(1)
+    byte_list.append(byte)
+    # print(byte.hex())
+    # print(int.from_bytes(byte,'big'))
 
 
 @with_goto
-def test():
+def test(count):
     # 1 open card file
     with open(path, "rb") as card:
-        print("step1")
+        # print("step1")
         # 2 read 4 bytes into byte_list
-        print("step2")
+        # print("step2")
 
         fill_byte_list(card, byte_list)
         # 3 compare with signature
-        print("step3")
-
         label .trig
-        if int(byte_list[0].hex()) == 0xff and int(byte_list[1].hex()) == 0xd8 and int(byte_list[2].hex()) == 0xff and int(byte_list[3].hex()) & 0xf0 == 0xe0:
+        # print("step3")
+        (byte1, byte2, byte3, byte4) = (int.from_bytes(byte_list[0], 'big'), int.from_bytes(
+            byte_list[1], 'big'), int.from_bytes(byte_list[2], 'big'), int.from_bytes(byte_list[3], 'big'))
+        if byte1 == 0xff and byte2 == 0xd8 and byte3 == 0xff and byte4 & 0xf0 == 0xe0:
             # 4if yes
-            print("step4")
+            # print("step4")
 
             label .new_image
 
             # 5open new image jpg
-            with open("{:3d}.jpg".format(count), "wb") as image:
-                print("step5")
-
+            # global count
+            with open("{:03d}.jpg".format(count), "wb") as image:
+                # print("step5")
+                # print(count)
                 count += 1
                 write_header_file(image, byte_list)
+                # print(byte_list)
                 # 6clear byte_list
 
-                byte_list.clear()
-                print("step6")
-
+                # byte_list.clear()
+                # print("step6")
+                # x = 0
                 label .continue_write
+                # x +=1
                 # 7write 512 bytes into image
-                image.write(card.read(512))
-                print("step7")
+                # print(x)
+                image.write(card.read(508))
+                padding = (4 - (512 * 24 % 4) % 4)
+
+                for _ in range(padding):
+                    # fputc(0x00, outptr);
+                    image.write(bytes(0x00) )
+                
+                # print("step7")
 
                 # 8 read 4 bytes into byte_list
 
                 fill_byte_list(card, byte_list)
-                print("step8")
+                # print(byte_list)
+                # print("step8")
                 # 9check end card file
 
                 if byte_list[0] == b'':
-                    goto .end
-                print("step9")
-                
+                    # goto .end
+                    return
+                # print("step9")
+
                 # 10 compare with signature
-                if int(byte_list[0].hex()) == 0xff and int(byte_list[1].hex()) == 0xd8 and int(byte_list[2].hex()) == 0xff and int(byte_list[3].hex()) & 0xf0 == 0xe0:
+
+                (byte1, byte2, byte3, byte4) = (int.from_bytes(byte_list[0], 'big'), int.from_bytes(
+                    byte_list[1], 'big'), int.from_bytes(byte_list[2], 'big'), int.from_bytes(byte_list[3], 'big'))
+                if byte1 == 0xff and byte2 == 0xd8 and byte3 == 0xff and byte4 & 0xf0 == 0xe0:
+                    # print("step10")
                     # 11 if yes go to step 5
                     goto .new_image
                 else:
@@ -80,12 +101,16 @@ def test():
             # 13 if no
             # 14 check end card file
             if byte_list[0] == b'':
-                goto .end
+                # goto .end
+                return
+            # print(byte_list)
+            # print("step14")
             # 15 drop first byte and append new byte (overlap)
             add_element_byte_list(card, byte_list)
+            # print("step15")
             # goto step 3
             goto .trig
-    label .end
+        # label .end
 
 
-test()
+test(count)
